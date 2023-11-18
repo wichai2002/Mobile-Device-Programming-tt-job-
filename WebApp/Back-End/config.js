fbef289cfc385b6a00f1f90967edce38760d2d3a
Wichai Kommongkhun <kommongkhun.wichai@gmail.com>
2023-11-18 14:59:34 +0700

    2023-11-18 14:59

--- WebApp/Back-End/config.js
+++ WebApp/Back-End/config.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
 
+// config your database
+
 const pool = mysql.createPool({
-  host: 'ttjob-project.ctyetg9uawza.ap-southeast-2.rds.amazonaws.com',
-  user: 'node_admin1',
-  password: '7t-8lopR9903f',
-  database: 'project_db',
+  host: 'your host name',
+  user: 'username',
+  password: '',
+  database: '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
